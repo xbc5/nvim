1a3dffc9e0fcdaa33d1168e32240b840c09d7f25
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-07-31 21:02:06 +0100

    feat(plugin): make rnvimr 95% window size
    Squashed commit of the following:
    
    commit 7384418c59c42730f00aa0cce191619347a0e232
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Sun Jul 31 21:01:32 2022 +0100
    
        remove redundant comment, borders do appear
    
    commit 7ced87513baaa5b454d99c2b13285bc8d4c9dd92
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Sun Jul 31 20:56:26 2022 +0100
    
        tweak layout
    
    commit f90c2716b02254851bc5b878144dc8f1b9c6589b
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Sun Jul 31 20:27:54 2022 +0100
    
        do initial resize

--- lua/config/rnvimr.lua
+++ lua/config/rnvimr.lua
@@ -8,13 +8,24 @@ function M.config()
   g('rnvimr_enable_bw', 1) -- delete buffer when file is deleted
 
   -- border
-  g('rnvimr_draw_border', 1) -- BUG: doesn't work
+  g('rnvimr_draw_border', 1)
   g('rnvimr_ranger_cmd', { 'ranger', '--cmd=set draw_borders both' }) -- inner borders
 
   -- keys
   local opts = { silent = true, noremap = true }
   map('n', '<A-o>', '<Cmd>RnvimrToggle<CR>', opts)
   map('t', '<A-o>', '<Cmd>RnvimrToggle<CR>', opts)
+
+  vim.cmd [[
+  let g:rnvimr_layout = {
+            \ 'relative': 'editor',
+            \ 'width': float2nr(round(0.96 * &columns)),
+            \ 'height': float2nr(round(0.96 * &lines)),
+            \ 'col': float2nr(round(0.02 * &columns)),
+            \ 'row': float2nr(round(0.02 * &lines)),
+            \ 'style': 'minimal'
+            \ }
+  ]]
 end
 
 return M
