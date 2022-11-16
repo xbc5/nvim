7ec05adac412181eae2cc8b92f68c275bf1350ca
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-11-16 22:47:28 +0000

    feat(plugin): reinstall dap-ui for useful popups

---
+++ lua/config/plugins/dap--nvim-dap-ui.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.setup(use)
+  use {
+    'rcarriga/nvim-dap-ui',
+    requires = {'mfussenegger/nvim-dap'},
+    config = function()
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      local map = vim.api.nvim_set_keymap
+      local opts = { silent = true, noremap = true }
+
+      map('n', '<leader>es',  '<Cmd>lua require("dapui").float_element("scopes", { enter = true })<CR>', opts)
+      map('n', '<leader>ev', '<Cmd>lua require("dapui").float_element("breakpoints", { enter = true })<CR>', opts)
+      map('n', '<leader>eS',  '<Cmd>lua require("dapui").float_element("stacks", { enter = true })<CR>', opts)
+      map('n', '<leader>ew',  '<Cmd>lua require("dapui").float_element("watches", { enter = true })<CR>', opts)
+      map('v', '<leader>ee',  '<Cmd>lua require("dapui").eval(nil, { enter = true })<CR>', opts)
+      map('n', '<leader>ee',  '<Cmd>lua require("dapui").eval(nil, { enter = true })<CR>', opts)
+    end
+  }
+end
+
+return M
