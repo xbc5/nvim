5b882cd517262dc1da594f31106e05f23b33a78c
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-10-27 09:16:03 +0100

    fix: broken lsp-saga

--- lua/config/plugins/ide--lspsaga.lua
+++ lua/config/plugins/ide--lspsaga.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup(use)
   use {
-    "glepnir/lspsaga.nvim",
+    "nvimdev/lspsaga.nvim",
     requires = {
       {"nvim-tree/nvim-web-devicons"},
       --Please make sure you install markdown and markdown_inline parser
@@ -10,19 +10,26 @@ function M.setup(use)
     },
 
     config = function()
-      require('lspsaga').setup({})
+      require('lspsaga').setup({
+        finder = { silent = true },
+        outline = { layout = "float" },
+      })
 
       local map = vim.keymap.set
       local opts = { silent = true, noremap = true }
 
       -- refactor
       map("n", "<leader>la", "<cmd>Lspsaga code_action<cr>", opts)
-      map("n", "<leader>ld",  "<cmd>Lspsaga hover_doc<cr>", opts)
-      map("n", "<leader>lf", "<cmd>Lspsaga lsp_finder<cr>", opts)
-      map("n", "<leader>li", "<cmd>Lspsaga implement<cr>", opts)
-      map("n", "<leader>lp",  "<cmd>Lspsaga preview_definition<cr>", opts)
+      map("n", "<leader>ld", "<cmd>Lspsaga hover_doc<cr>", opts)
+      map("n", "<leader>lf", "<cmd>Lspsaga finder tyd+def+ref+imp<cr>", opts)
+      map("n", "<leader>lp", "<cmd>Lspsaga peek_definition<cr>", opts)
       map("n", "<leader>lr", "<cmd>Lspsaga rename<cr>", opts)
-      map("n", "<leader>ls",  "<cmd>Lspsaga signature_help<cr>", opts)
+      map("n", "<leader>lo", "<cmd>Lspsaga outline<cr>", opts)
+      map("n", "<leader>lb", "<cmd>Lspsaga show_buf_diagnostics<cr>", opts)
+      map("n", "<leader>lw", "<cmd>Lspsaga show_workspace_diagnostics ++float<cr>", opts)
+      map("n", "<leader>lx", "<cmd>Lspsaga show_cursor_diagnostics<cr>", opts)
+      map("n", "<leader>lci", "<cmd>Lspsaga incoming_calls<cr>", opts)
+      map("n", "<leader>lco", "<cmd>Lspsaga outgoing_calls<cr>", opts)
       map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
       map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
     end
