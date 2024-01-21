a93258509389cd9915ec0335117a95dc330d7016
xbc5 <87829033+xbc5@users.noreply.github.com>
2024-01-21 17:30:20 +0000

    fix: make Fidget notifications work

--- lua/config/plugins/sys--fidget.lua
+++ lua/config/plugins/sys--fidget.lua
@@ -4,6 +4,18 @@ function M.setup(use)
   use {
     "j-hui/fidget.nvim",
     config = function()
+      local f = require("fidget")
+      if f then
+        f.setup {
+          notification = {
+            override_vim_notify = true, -- enables it
+          }
+        }
+
+        vim.api.nvim_set_keymap(
+          "n", "<leader>vn", '<cmd>Fidget history<cr>', { noremap = true }
+        )
+      end
     end
   }
 end
