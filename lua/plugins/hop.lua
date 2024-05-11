85fdb7bd1d4c45cfe6089731a32122d510024095
xbc5 <87829033+xbc5@users.noreply.github.com>
2024-05-12 00:34:57 +0100

    fix: broken hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,9 +1,10 @@
 return {
   {
     "phaazon/hop.nvim",
+    cmd = "HopWord",
     config = function()
       require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-      require("lib.nvim").map("n", ":<leader>hw", ":HopWord", { desc = "Hop Word" })
+      require("lib.nvim").map("n", "<leader>hw", ":HopWord<CR>", { desc = "Hop Word" })
     end,
   },
 }
