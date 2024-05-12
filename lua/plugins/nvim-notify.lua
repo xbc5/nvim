81fc806cb8afb3282ebe44cf399b6e0554c6e846
xbc5 <87829033+xbc5@users.noreply.github.com>
2024-05-12 17:17:13 +0100

    refactor: tidy nvim-notify config

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,15 +1,15 @@
 return {
   "rcarriga/nvim-notify",
   enabled = false,
-  require = "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    stages = "static",
+  },
+  keys = {
+    { "<leader>fn", ":Telescope notify<CR>", { desc = "Fuzzy Notifications" } },
+    { "<leader>vn", ":Notifications<CR>" },
+  },
   config = function()
-    local notify = require("notify")
-    notify.setup()
-    vim.notify = notify
-
     require("telescope").load_extension("notify")
-    local opts = { silent = true, noremap = true }
-    vim.keymap.set("n", "<leader>fn", ":Telescope notify<CR>", opts)
-    vim.keymap.set("n", "<leader>vn", ":Notifications<CR>", opts)
   end,
 }
