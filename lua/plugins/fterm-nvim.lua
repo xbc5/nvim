4f2320e474312a63406795c38d197931f4d1cba5
xbc5 <87829033+xbc5@users.noreply.github.com>
2024-05-13 06:42:34 +0100

    fix: make rnvimr and fterm load on init

--- lua/plugins/fterm-nvim.lua
+++ lua/plugins/fterm-nvim.lua
@@ -1,12 +1,13 @@
 return {
   {
-    'numtostr/FTerm.nvim',
+    "numtostr/FTerm.nvim",
+    lazy = false,
     config = function()
       local table = require("lib.table")
       local fterm = require("FTerm")
 
       local defaults = {
-        dimensions  = {
+        dimensions = {
           height = 0.95,
           width = 0.95,
         },
@@ -17,10 +18,11 @@ return {
       local lazygit = fterm:new(table.merge(defaults, { cmd = "lazygit" }))
 
       local opts = { silent = true, noremap = true }
-      vim.keymap.set('n', '<leader>gg', function() lazygit:toggle() end, opts)
-      vim.keymap.set('n', '<A-Space>', require("FTerm").toggle, opts)
-      vim.keymap.set('t', '<A-Space>', require("FTerm").toggle, opts)
-
-    end
-  }
+      vim.keymap.set("n", "<leader>gg", function()
+        lazygit:toggle()
+      end, opts)
+      vim.keymap.set("n", "<A-Space>", require("FTerm").toggle, opts)
+      vim.keymap.set("t", "<A-Space>", require("FTerm").toggle, opts)
+    end,
+  },
 }
--- lua/plugins/rnvimr.lua
+++ lua/plugins/rnvimr.lua
@@ -1,23 +1,24 @@
 return {
   {
-    'kevinhwang91/rnvimr',
+    "kevinhwang91/rnvimr",
+    lazy = false,
     config = function()
       local map = vim.api.nvim_set_keymap
       local g = vim.api.nvim_set_var
 
-      g('rnvimr_enable_ex', 1) -- replace buitin FM (Netrw)
-      g('rnvimr_enable_bw', 1) -- delete buffer when file is deleted
+      g("rnvimr_enable_ex", 1) -- replace buitin FM (Netrw)
+      g("rnvimr_enable_bw", 1) -- delete buffer when file is deleted
 
       -- border
-      g('rnvimr_draw_border', 1)
-      g('rnvimr_ranger_cmd', { 'ranger', '--cmd=set draw_borders both' }) -- inner borders
+      g("rnvimr_draw_border", 1)
+      g("rnvimr_ranger_cmd", { "ranger", "--cmd=set draw_borders both" }) -- inner borders
 
       -- keys
       local opts = { silent = true, noremap = true }
-      map('n', '<A-o>', '<Cmd>RnvimrToggle<CR>', opts)
-      map('t', '<A-o>', '<Cmd>RnvimrToggle<CR>', opts)
+      map("n", "<A-o>", "<Cmd>RnvimrToggle<CR>", opts)
+      map("t", "<A-o>", "<Cmd>RnvimrToggle<CR>", opts)
 
-      vim.cmd [[
+      vim.cmd([[
       let g:rnvimr_layout = {
         \ 'relative': 'editor',
         \ 'width': float2nr(round(0.96 * &columns)),
@@ -26,7 +27,7 @@ return {
         \ 'row': float2nr(round(0.02 * &lines)),
         \ 'style': 'minimal'
         \ }
-        ]]
-    end
-  }
+        ]])
+    end,
+  },
 }
