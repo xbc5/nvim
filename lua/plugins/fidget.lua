f6935852e6f8b0a6d16c8c0bac7347fc5dae5061
xbc5 <87829033+xbc5@users.noreply.github.com>
2024-05-13 06:36:50 +0100

    refactor: change silly null check

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -4,16 +4,13 @@ return {
   {
     "j-hui/fidget.nvim",
     config = function()
-      local f = require("fidget")
-      if f then
-        f.setup({
-          notification = {
-            override_vim_notify = true, -- enables it
-          },
-        })
+      require("fidget").setup({
+        notification = {
+          override_vim_notify = true, -- enables it
+        },
+      })
 
-        map("n", "<leader>vn", "<cmd>Fidget history<cr>", { desc = "Notification Viewer" })
-      end
+      map("n", "<leader>vn", "<cmd>Fidget history<cr>", { desc = "Notification Viewer" })
     end,
   },
 }
