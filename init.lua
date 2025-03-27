a42fbb7a727935f6fc73118f7cdbdd4295f2e652
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-03-27 10:25:21 +0000

    feat: create Python import organiser autocmd

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_augroup("PyrightOrganizeImports", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = "PyrightOrganizeImports",
+  pattern = "*.py",
+  command = "silent! PyrightOrganizeImports",
+})
