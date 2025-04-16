031246364be12aa3752367e2490f8e2be65c5cf9
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-04-16 18:24:30 +0100

    feat: add Typst Tree-sitter grammar

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -19,62 +19,77 @@ return {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-      sync_install = false,
-      auto_install = true,
-      -- nvim-treesitter-textobjects
-      textobjects = {
-        select = {
-          enable = true, -- WARN: all modules disabled by default
-          include_surrounding_whitespace = true,
-          keymaps = {
-            -- capture groups defined in ~/.local/.../packer/nvim-treesitter/captures/*
-            -- see :h nvim-treesitter-incremental-selection-mod
-            ["aa"] = "@parameter.outer", -- a: "arg" (p is paragraph)
-            -- ["ia"] = "@parameter.inner", -- doesn't work so well
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["aC"] = "@comment.outer",
-            ["iC"] = "@comment.inner", -- doesn't work
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["al"] = "@loop.outer",
-            ["il"] = "@loop.inner",
+    opts = function(_, opts)
+      -- Deep merge parent opts with new overrides.
+      local new_opts = vim.tbl_deep_extend("force", opts, {
+        sync_install = false,
+        auto_install = true,
+        textobjects = {
+          select = {
+            enable = true, -- WARN: all modules disabled by default
+            include_surrounding_whitespace = true,
+            keymaps = {
+              -- capture groups defined in ~/.local/.../packer/nvim-treesitter/captures/*
+              -- see :h nvim-treesitter-incremental-selection-mod
+              ["aa"] = "@parameter.outer", -- a: "arg" (p is paragraph)
+              -- ["ia"] = "@parameter.inner", -- doesn't work so well
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aC"] = "@comment.outer",
+              ["iC"] = "@comment.inner", -- doesn't work
+              ["ab"] = "@block.outer",
+              ["ib"] = "@block.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+            },
+          },
+          swap = {
+            enable = true,
+            -- swap the node under the cursor: e.g. param, function
+            swap_next = {
+              ["<C-J>"] = "@parameter.inner",
+            },
+            swap_previous = {
+              ["<C-K>"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- true: add to jump list
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]x"] = "@class.outer",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]X"] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[x"] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[X"] = "@class.outer",
+            },
           },
         },
-        swap = {
-          enable = true,
-          -- swap the node under the cursor: e.g. param, function
-          swap_next = {
-            ["<C-J>"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<C-K>"] = "@parameter.inner",
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- true: add to jump list
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]x"] = "@class.outer",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]X"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[x"] = "@class.outer",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[X"] = "@class.outer",
-          },
-        },
-      },
-    },
+      })
+
+      -- Extend this list, because LazyVim only merges keyed tables[0].
+      new_opts.ensure_installed = new_opts.ensure_installed or {}
+      new_opts.ensure_installed = vim.list_extend(new_opts.ensure_installed, {
+        "typst", -- For the Typst typsetting DSL.
+      })
+
+      return new_opts
+    end,
   },
 }
+
+--[[
+-- References
+--   [0]https://github.com/folke/lazy.nvim/discussions/1706#discussioncomment-10268907
+--]]
