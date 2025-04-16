c01072476e9a87ee41adccfec83e2e0a3d80b327
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-04-16 19:20:30 +0100

    fix: vim-illuminate LSP error
    Disable it for now.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -10,7 +10,7 @@ return {
   { "nvim-neo-tree/neo-tree.nvim" }, -- WARN: overrides DAP keymap 'e'
   { "vim-scripts/loremipsum", cmd = "Loremipsum" },
   { "folke/which-key.nvim", event = "VeryLazy" }, -- see keymaps.lua for register() keymaps
-  { "RRethy/vim-illuminate", event = "BufEnter" },
+  { "RRethy/vim-illuminate", event = "BufEnter", enabled = false }, -- Some LSP bug (c. Apr. 2025)[0]; I don't use, so I don't care.
   { "b3nj5m1n/kommentary", event = "BufEnter" },
   { "tpope/vim-obsession", cmd = "Obsession" },
   {
@@ -30,3 +30,8 @@ return {
     },
   },
 }
+
+--[[
+-- References
+--   [0] https://github.com/RRethy/vim-illuminate/issues/232
+--]]
