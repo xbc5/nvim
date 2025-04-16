af63c9fb2d475e73d5162fac2418c38a4287773d
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-04-16 18:54:56 +0100

    refactor: version control all Mason servers

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,23 +2,67 @@ return {
   {
     "williamboman/mason.nvim",
     opts = {
-      -- I think ensure_installed is a LazyVim extension due to this line in the full spec:
+      -- I think ensure_installed is a LazyVim extension, due to this line in the full spec:
       -- `opts_extend = { "ensure_installed" }`
       -- However, Mason does support a similar key via it's companion package: mason-lspconfig.nvim[0].
       -- Using LazyVim's implementation for now.
       ensure_installed = {
-        -- Writing.
-        "tinymist", -- Typst.
+        -- Build.
+        "cmakelang",
+        "cmakelint",
+        "neocmakelsp",
 
-        -- Web
-        "emmet-ls",
-        "html-lsp",
-        "css-lsp",
+        -- Config.
+        "json-lsp",
+        "kcl", -- That CNCF config language.
+        "yaml-language-server",
 
-        -- Other
-        "bash-language-server",
+        -- Docker.
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "hadolint",
+
+        -- Go
+        "delve",
+        "gofumpt",
+        "goimports",
+        "golines",
+        "gopls",
+
+        -- Other.
+        "lua-language-server",
         "rust-analyzer",
+        "stylua",
         "vim-language-server",
+
+        -- Python.
+        "black",
+        "debugpy",
+        "pyright",
+        "ruff",
+        "ruff-lsp",
+
+        -- Shell.
+        "bash-language-server",
+        "shfmt",
+
+        -- Web.
+        "biome", -- TypeScript, JavaScript.
+        "css-lsp",
+        "emmet-ls",
+        "eslint-lsp",
+        "html-lsp",
+        "js-debug-adapter",
+        "prettierd",
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "vtsls", -- TypeScript.
+
+        -- Writing.
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman", -- Markdown.
+        "tinymist", -- Typst.
       },
     },
   },
