ff6a5ff07863091626a89aa5b59b7ee9f274d49a
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-10-10 22:36:56 +0100

    refactor: remove custom gitsigns config
    This overrides the `LazyVim` config, and it give me access
    back to manipulating hunks.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,36 +4,6 @@ return {
     event = { "BufEnter" },
     opts = {
       current_line_blame = false, -- HUGE performance hog, laggy input
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Cannot map these to :Gtsigns next|preview_hunk becuase they don't work.
-        map("n", "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
-
-        map("n", "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
-      end,
     },
   },
 }
